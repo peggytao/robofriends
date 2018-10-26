8673a2d4fa5a6aba74701af40442a0822c448dc1
Peggy Tao <peggytao@hotmail.com>
2018-10-25 20:56:46 -0400

    Updates

---
+++ precache-manifest.8d53ae6ea6a467f5c22f6e9d83638b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "a34688d3cca17032cc80",
+    "url": "/robofriends/static/js/main.a34688d3.chunk.js"
+  },
+  {
+    "revision": "f1523ca30fa5a82477e3",
+    "url": "/robofriends/static/js/1.f1523ca3.chunk.js"
+  },
+  {
+    "revision": "a34688d3cca17032cc80",
+    "url": "/robofriends/static/css/main.8efc2bbf.chunk.css"
+  },
+  {
+    "revision": "f1523ca30fa5a82477e3",
+    "url": "/robofriends/static/css/1.b9aeb236.chunk.css"
+  },
+  {
+    "revision": "70acc5200e256af42827f604992545a6",
+    "url": "/robofriends/index.html"
+  }
+];
